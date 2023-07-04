115d08149e3561ef4746e01c665661920bae5648
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-04 15:53:26 -0300

    DE1 Concluida

--- d1_de1.dart
+++ d1_de1.dart
@@ -1,1 +1,25 @@
-void main() {}
+// Tempo total: 6:23,10
+
+import 'dart:math';
+
+void main() {
+  final listaRaios = [5, 8, 12, 7.3, 18, 2, 25];
+
+  for (final raio in listaRaios) {
+    Circulo r = Circulo();
+    print(
+        "Raio: $raio Area: ${r.area(raio).toStringAsFixed(2)} Perimetro: ${r.perimetro(raio).toStringAsFixed(2)} ");
+  }
+}
+
+class Circulo {
+  double area(raio) {
+    final area = pi * (raio * raio);
+    return area;
+  }
+
+  double perimetro(raio) {
+    final perimetro = 2 * pi * raio;
+    return perimetro;
+  }
+}
