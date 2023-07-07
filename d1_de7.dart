43f5da1ffb245c7f3af560bfaa7cdca442cda114
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-07 08:50:58 -0300

    DE7 Concluida

--- d1_de7.dart
+++ d1_de7.dart
@@ -1,1 +1,37 @@
-void main() {}
+void main() {
+  final listaNumeros = [
+    3,
+    17,
+    23,
+  ];
+  print(somaMetodoLista(listaNumeros));
+}
+
+int somaFor(List<int> listaNumeros) {
+  int soma = 0;
+  for (final numero in listaNumeros) {
+    soma += numero;
+  }
+  return soma;
+}
+
+int somaWhile(List<int> listaNumeros) {
+  int soma = 0;
+  int i = 0;
+  while (i < listaNumeros.length) {
+    soma += listaNumeros[i];
+    i++;
+  }
+  return soma;
+}
+
+int somaRecursivo(List<int> listaNumeros) {
+  if (listaNumeros.isEmpty) {
+    return 0;
+  }
+  return listaNumeros.first + somaRecursivo(listaNumeros.sublist(1));
+}
+
+int somaMetodoLista(List<int> listaNumeros) {
+  return listaNumeros.reduce((x, y) => x + y);
+}
