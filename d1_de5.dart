9a7a16f86a3cedd7632a91df0b0a1f712e5a08ab
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-07 07:43:29 -0300

    DE5 Concluida

--- d1_de5.dart
+++ d1_de5.dart
@@ -1,1 +1,58 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final random = Random();
+  final listaNomes = [
+    'Ana',
+    'Maria',
+    'Francisco',
+    'Joao',
+    'Pedro',
+    'Gabriel',
+    'Rafaela',
+    'Marcio',
+    'Jose',
+    'Carlos',
+    'Patricia',
+    'Helena',
+    'Camila',
+    'Mateus',
+    'Gabriel',
+    'Samuel',
+    'Karina',
+    'Antonio',
+    'Daniel',
+    'Joel',
+    'Cristiana',
+    'Sebastiao',
+    'Paula'
+  ];
+  final listaSobrenomes = [
+    'Silva',
+    'Souza',
+    'Almeida',
+    'Azevedo',
+    'Braga',
+    'Barros',
+    'Campos',
+    'Cardoso',
+    'Costa',
+    'Teixeira',
+    'Santos',
+    'Rodrigues',
+    'Ferreira',
+    'Alves',
+    'Pereira',
+    'Lima',
+    'Gomes',
+    'Ribeiro',
+    'Carvalho',
+    'Lopes',
+    'Barbosa'
+  ];
+  final nomeGerado = listaNomes[random.nextInt(listaNomes.length)];
+  final sobrenomeGerado =
+      listaSobrenomes[random.nextInt(listaSobrenomes.length)];
+
+  print("Nome gerado: $nomeGerado $sobrenomeGerado");
+}
