e4e8a3560a50b77fbedd00f91541b00da3447281
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-07 08:07:51 -0300

    DE6 Concluida

--- d1_de6.dart
+++ d1_de6.dart
@@ -1,1 +1,15 @@
-void main() {}
+void main() {
+  final numeros = [3, 17, 23, 49, 328, 1358, 21, 429, 12, 103, 20021];
+  converteNumeros(numeros);
+}
+
+void converteNumeros(List<int> listaNumeros) {
+  listaNumeros.sort();
+  for (final decimal in listaNumeros) {
+    final binario = decimal.toRadixString(2);
+    final octal = decimal.toRadixString(8);
+    final hexadecimal = decimal.toRadixString(16);
+    print(
+        "Decimal: $decimal, Binário: $binario, Octal: $octal, Hexadecimal: $hexadecimal");
+  }
+}
